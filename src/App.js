8aad098569535f84b2dee6cd60a6ef2a39a31968
Jason Chen <jason.l.chen@gmail.com>
2019-10-29 16:31:35 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container-fluid'>
+        <div className='col-sm'>
+          <div className='dropdown'>
+            <button
+              className='btn btn-secondary dropdown-toggle dropdown-level-one'
+              type='button'
+              id='dropdownMenuButton'
+              data-toggle='dropdown'
+              aria-haspopup='true'
+              aria-expanded='false'
+            >
+              Dropdown button
+            </button>
+            <div className='dropdown-menu' aria-labelledby='dropdownMenuButton'>
+              <a className='dropdown-item dropdown-level-two' href='#'>
+                Action
+              </a>
+              <a className='dropdown-item' href='#'>
+                Another action
+              </a>
+              <a className='dropdown-item' href='#'>
+                Something else here
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
