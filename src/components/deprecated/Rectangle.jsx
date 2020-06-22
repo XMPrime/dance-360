22055b386cdcf935e5ebf4c59b8387eb4c5f3b0d
Jason Chen <jason.l.chen@gmail.com>
2020-06-22 15:31:28 -0700

    Refactored to use "self-calling" (?) object
    see line 22-26

--- src/components/deprecated/Rectangle.jsx
+++ src/components/deprecated/Rectangle.jsx
@@ -10,30 +10,6 @@ export default function Rectangle({
   text,
 }) {
   const [grade, setGrade] = useState('neutral');
-  const [level4id, setLevel4id] = useState(level_4_id);
-  const [level1id, setLevel1id] = useState(level_1_id);
-
-  function goodToggle(e) {
-    e.preventDefault();
-    // TODO do not need switch statement here
-    // switch (grade) {
-    //   case 'good':
-    //     setGrade('bad');
-    //     break;
-    //   case 'bad':
-    //     setGrade('neutral');
-    //     break;
-    //   default:
-    //     setGrade('good');
-    //     break;
-    // }
-    setGrade(
-      {
-        good: 'bad',
-        bad: 'neutral',
-      }[grade] || 'good',
-    );
-  }
 
   return (
     <div
@@ -41,7 +17,15 @@ export default function Rectangle({
       level_1_id={level_1_id}
       grade={grade}
       className={`rectangle level_${isHeader ? level : 4} ${grade}`}
-      onClick={!isHeader && goodToggle}
+      onClick={
+        !isHeader &&
+        setGrade(
+          {
+            good: 'bad',
+            bad: 'neutral',
+          }[grade] || 'good',
+        )
+      }
       role="button"
       tabIndex={0}
     >
